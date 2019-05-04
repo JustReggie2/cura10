88b7d10d602cc8652957c4264bb9413321029a51
github username <github email address>
2019-05-04 14:29:31 -0400

    updated lists routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists
+  resources :lists, only: [:index, :show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lists#index'
 end
