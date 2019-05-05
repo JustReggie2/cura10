ba668e36e1399eb602d3a2d8e5c210ace4b53464
github username <github email address>
2019-05-04 23:29:19 -0400

    add setter method for category

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,11 @@
 class List < ApplicationRecord
   belongs_to :category, optional: true
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name: name)
+  end
+
+  def category_name
+    self.category ? self.category.name : nil
+  end
 end
