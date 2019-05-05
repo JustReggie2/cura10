3c6f4a3ac319ff244cd16483466dbb41571a0132
github username <github email address>
2019-05-05 19:41:24 -0400

    add accepts_nested_attributes_for

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   belongs_to :category, optional: true
   has_many :items
+  accepts_nested_attributes_for :items 
 
   def category_name=(name)
     self.category = Category.find_or_create_by(name: name)
