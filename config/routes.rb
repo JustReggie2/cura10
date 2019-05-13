dbe0b42c00582c1013bf5ba44fe7efe42be5ea5c
github username <github email address>
2019-05-13 19:47:30 -0400

    add resource user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :lists, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :users, only: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lists#index'
 end
