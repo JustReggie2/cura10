82352bf3a5599a1944c0feac5777eb3a125db767
github username <github email address>
2019-05-13 21:19:50 -0400

    add create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,17 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     return redirect_to new_user_path unless @user.save
-    redirect_to
+    redirect_to user_path(@user)
   end
 
 
   def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
