66f5c1d52a94a1fb1633772937248b371dd61e85
github username <github email address>
2019-05-13 21:03:30 -0400

    changed all index to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
 
 
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    return redirect_to new_user_path unless @user.save
+    redirect_to
   end
 
 
-  def index
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   resources :lists, only: [:index, :show, :new, :create, :edit, :update]
 
-  resources :users, only: [:new, :create, :index]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lists#index'
 end
