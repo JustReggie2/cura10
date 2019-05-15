0283ad248c4c19267423b961e562c9ee4e82c564
github username <github email address>
2019-05-14 20:51:02 -0400

    add create for logging in user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # raise params.inspect
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to login_path unless user
+    @user = user
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   def destroy
