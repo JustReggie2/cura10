b2b67bd89ab16b97a2b61bad6b0e1b1c5be4aa72
github username <github email address>
2019-05-14 20:24:10 -0400

    add login/logout routes for sessionscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   resources :lists, only: [:index, :show, :new, :create, :edit, :update]
 
   resources :users, only: [:new, :create, :show]
