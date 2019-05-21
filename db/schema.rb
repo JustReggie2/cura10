7e5423bc58dccb3584998195702a1b94ff94ff7b
github username <github email address>
2019-05-21 17:40:28 -0400

    add column user_id to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_212053) do
+ActiveRecord::Schema.define(version: 2019_05_21_213922) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_212053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.integer "user_id"
   end
 
   create_table "user_lists", force: :cascade do |t|
