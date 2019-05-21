5223a033c771354d570130152e81f6b35ab412b6
github username <github email address>
2019-05-21 17:57:28 -0400

    dropped user_lists from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_215015) do
+ActiveRecord::Schema.define(version: 2019_05_21_215600) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -39,14 +39,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_215015) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "user_id"
-  end
-
-  create_table "user_lists", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "list_id"
-    t.integer "vote"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
