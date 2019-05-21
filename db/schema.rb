912aa51093ecc8ce6f1138a2878bc38570951388
github username <github email address>
2019-05-21 17:31:56 -0400

    add model/migration for user_lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_222937) do
+ActiveRecord::Schema.define(version: 2019_05_21_212053) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2019_05_15_222937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+  end
+
+  create_table "user_lists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "list_id"
+    t.integer "vote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
