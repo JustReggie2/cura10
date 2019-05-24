278da2ee8b4ff31637e1eccccea68ef31f4e4000
github username <github email address>
2019-05-23 22:42:44 -0400

    add favorited methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   has_many :favorite_lists
   has_many :favorites, through: :favorite_lists, source: :list
   has_secure_password
+
+  def favorited!(list)
+    self.favorites << list
+  end
+
+  def unfavorited!(list)
+    self.favorites.delete(list)
+  end
+
+  def favorited?(list)
+    self.favorites.find_by(id: list.id)
+  end
 end
