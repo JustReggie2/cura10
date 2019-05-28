b7d56ba9881e20f2879546715d8da6acd7c90d02
github username <github email address>
2019-05-28 19:52:02 -0400

    add name & email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :lists
   has_many :favorite_lists
   has_many :favorites, through: :favorite_lists, source: :list
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   has_secure_password
 
   def favorited!(list)
