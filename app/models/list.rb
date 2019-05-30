83c0eeeffb4c7bc8caacfc4dfea3777cb99044c8
github username <github email address>
2019-05-30 17:26:53 -0400

    commented out vote system changes for the moment

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,16 +4,8 @@ class List < ApplicationRecord
   has_many :items
   has_many :favorite_lists
   has_many :favorited_by, through: :favorite_lists, source: :user
+  # has_many :votes, through: :favorite_lists, source: :user
   accepts_nested_attributes_for :items
-  # def item_desc_links=(items)
-  #   items.each do |desc_link|
-  #     item = self.items.build(desc_link: desc_link) unless desc_link.empty?
-  #   end
-  # end
-  #
-  # def item_desc_links
-  #   self.items.collect(&:desc_link)
-  # end
 
   def category_name=(name)
     self.category = Category.find_or_create_by(name: name)
@@ -22,4 +14,6 @@ class List < ApplicationRecord
   def category_name
     self.category ? self.category.name : nil
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :lists
   has_many :favorite_lists
   has_many :favorites, through: :favorite_lists, source: :list
+  # has_many :voted_for, through: :favorite_lists, source: :list
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "is invalid" }
   validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must be at least 6 characters and include one number & one letter"}
@@ -18,4 +19,8 @@ class User < ApplicationRecord
   def favorited?(list)
     self.favorites.find_by(id: list.id).present?
   end
+
+  # def up_vote(list)
+  #   list.votes = list.votes +1
+  # end
 end
