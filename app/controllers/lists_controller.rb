5b08300e7fffc15e6f0639d7e7593b2cd74c3a57
github username <github email address>
2019-06-10 20:09:23 -0400

    add up/down vote actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -40,6 +40,41 @@ class ListsController < ApplicationController
     end
   end
 
+  def up_vote
+    @user = current_user
+    @list = List.find(params[:id])
+    if @user.favorited?(@list)
+      @fav_vote = FavoriteList.find_by(user_id: @user.id, list_id: @list.id)
+      if @fav_vote.vote == nil || @fav_vote.vote == 0
+        @fav_vote.vote = 1
+        @fav_vote.save
+        redirect_to list_path(@list), notice: "Vote recorded!"
+      else
+        redirect_to list_path(@list), alert: "Unable to vote, perhaps you already did."
+      end
+    else
+      redirect_to list_path(@list), alert: "You must favorite list to to vote."
+    end
+  end
+
+  def down_vote
+    @user = current_user
+    @list = List.find(params[:id])
+    if @user.favorited?(@list)
+      @fav_vote = FavoriteList.find_by(user_id: @user.id, list_id: @list.id)
+      if @fav_vote.vote == 1
+        @fav_vote.vote -=1
+        @fav_vote.save
+        redirect_to list_path(@list), notice: "Vote recorded!"
+      else
+        redirect_to list_path(@list), alert: "Unable to vote, perhaps you already did."
+      end
+    else
+      redirect_to list_path(@list), alert: "You must favorite list to to vote."
+    end
+  end
+
+
   private
 
   def list_params
