075eee0efe4282ce52c862b473fd18b86eecd130
github username <github email address>
2019-06-16 16:08:26 -0400

    add has_votes & most_votes scope methods

--- app/models/list.rb
+++ app/models/list.rb
@@ -16,7 +16,7 @@ class List < ApplicationRecord
     self.category ? self.category.name : nil
   end
 
-  
+
   def fav_count
     self.favorited_by.count
   end
@@ -25,4 +25,16 @@ class List < ApplicationRecord
     self.votes.count
   end
 
+  scope :has_votes, -> { joins(:favorite_lists).where('vote = ?', 1) }
+  scope :most_votes, -> { has_votes.group(:list_id).order('count(list_id) DESC').limit(10) }
+  # scope :most_votes, -> { has_votes.order('list_id DESC').group(:list_id).count }
+  # def self.count_votes
+  #   joins(:favorite_lists).group(:list_id).select("vote, count(*) as total_votes")
+  # end
+  # def self.most_votes
+  #   where("vote >= 2")
+  # end
+
+  # scope :most_votes, -> {where(list_id: self.maximum(:vote))}
+  # I want to figure out the list_id that has the largest vote count...
 end
