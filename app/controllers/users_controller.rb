7abb452981d1823f34564219334684738c6d8439
github username <github email address>
2019-06-24 20:29:22 -0400

    add user login in create action when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     return render :new unless @user.save
+    session[:user_id] = @user.id
     redirect_to user_path(@user)
   end
 
