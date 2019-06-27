5f8671d4f5e4977c4e9f5da61e0cbbb9d10b59af
github username <github email address>
2019-06-27 09:37:19 -0400

    add item limit validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,12 @@
 class List < ApplicationRecord
   belongs_to :user
   belongs_to :category, optional: true
-  has_many :items
+  has_many :items, before_add: :validate_item_limit
   has_many :favorite_lists
   has_many :favorited_by, through: :favorite_lists, source: :user
   has_many :votes, through: :favorite_lists, source: :user
   validates :name, presence: true
+  NUMBER_OF_PERMITTED_ITEMS = 10
   accepts_nested_attributes_for :items
   scope :has_votes, -> { joins(:favorite_lists).where('vote = ?', 1) }
   scope :most_votes, -> { has_votes.group(:list_id).order('count(list_id) DESC').limit(10) }
@@ -27,6 +28,10 @@ class List < ApplicationRecord
     self.votes.count
   end
 
+  private
 
+  def validate_item_limit(item)
+    raise Exception.new if items.size >= NUMBER_OF_PERMITTED_ITEMS
+  end
 
 end
